9e11656b9a89bba25479fa90f608ff6f697a0371
ErnestinaAnkrah <ankrahtyna22@gmail.com>
2023-04-23 20:30:25 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Prints out the last part of a quote in standard error
+ *
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
